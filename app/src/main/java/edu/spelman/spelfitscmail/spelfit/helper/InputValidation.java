aaaf527a300d651aea21a27c8443684f56ab7bb6
meera436 <amirah.husbands@gmail.com>
2018-11-08 02:36:10 -0500

    set methods to validate input ie email password etc

--- app/src/main/java/edu/spelman/spelfitscmail/spelfit/helper/InputValidation.java
+++ app/src/main/java/edu/spelman/spelfitscmail/spelfit/helper/InputValidation.java
@@ -1,7 +1,10 @@
 package edu.spelman.spelfitscmail.spelfit.helper;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.Patterns;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 public class InputValidation {
 
@@ -34,5 +37,23 @@ public class InputValidation {
             textInputLayout.setErrorEnabled(false);
         }
         return true;
+    }
+
+    public boolean isInputEditTextMatches(TextInputEditText textInputEditText1, TextInputEditText textInputEditText2, TextInputLayout textInputLayout, String message){
+        String value1 = textInputEditText1.getText().toString().trim();
+        String value2 = textInputEditText2.getText().toString().trim();
+        if (!value1.contentEquals(value2)){
+            textInputLayout.setError(message);
+            hideKeyboardFrom(textInputEditText2);
+            return false;
+        } else{
+            textInputLayout.setErrorEnabled(false);
+        }
+        return true;
+    }
+    private void hideKeyboardFrom(View view){
+        InputMethodManager imm =(InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
     }
 }
--- app/src/main/java/edu/spelman/spelfitscmail/spelfit/sql/DatabaseHelper.java
+++ app/src/main/java/edu/spelman/spelfitscmail/spelfit/sql/DatabaseHelper.java
@@ -1,7 +1,45 @@
 package edu.spelman.spelfitscmail.spelfit.sql;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "UserManager.db";
+
+    private static final String TABLE_USER ="user";
+
+    private static final String COLUMN_USER_ID = "user_id";
+
+    private static final String COLUMN_USER_NAME = "user_name";
+
+    private static final String COLUMN_USER_EMAIL = "user_email";
+
+    private static final String COLUMN_USER_PASSWORD = "user_password";
+
+    private String CREATE_USER_TABLE = "CREATE TABLE" + TABLE_USER + "("
+            + COLUMN_USER_ID + "INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_USER_NAME + "TEXT,"
+            + COLUMN_USER_EMAIL + "TEXT," +COLUMN_USER_PASSWORD +"TEXT," + ")";
+
+    private String DROP_USER_TABLE = "DROP TABLE IF EXISTS" + TABLE_USER;
+
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(CREATE_USER_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL(DROP_USER_TABLE);
+        onCreate(db);
+    }
 
 
 
