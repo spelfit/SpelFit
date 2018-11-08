70cf40e3bc0277bfa3f08a6d13d2e0c8a0df0cac
meera436 <amirah.husbands@gmail.com>
2018-11-08 02:05:27 -0500

    set methods to validate input ie email password etc

--- app/src/main/java/edu/spelman/spelfitscmail/spelfit/helper/InputValidation.java
+++ app/src/main/java/edu/spelman/spelfitscmail/spelfit/helper/InputValidation.java
@@ -1,4 +1,38 @@
 package edu.spelman.spelfitscmail.spelfit.helper;
 
+import android.content.Context;
+import android.util.Patterns;
+
 public class InputValidation {
+
+    private Context context;
+
+    public InputValidation(Context context) {
+        this.context = context;
+    }
+
+    public boolean isinputEditTextFilled(TextInputEditText textInputEditText, TextInputLayout textInputLayout, String message) {
+
+        String value = textInputEditText.getText().toString().trim();
+        if (value.isEmpty()) {
+            textInputLayout.setError(message);
+            hideKeyboardFrom(textInputEditText);
+            return false;
+        } else{
+            textInputLayout.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+    public boolean isInputEditTextEmail(TextInputEditText textInputEditText, TextInputLayout textInputLayout, String message){
+        String value = textInputEditText.getText().toString().trim();
+        if (value.isEmpty() || Patterns.EMAIL_ADDRESS.matcher(value.matches())){
+            textInputLayout.setError(message);
+            hideKeyboardFrom(textInputEditText);
+            return false;
+        } else{
+            textInputLayout.setErrorEnabled(false);
+        }
+        return true;
+    }
 }
