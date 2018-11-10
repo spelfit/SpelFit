ec4aa8dc7ce7b623c406ceee07cb35b19c54938a
meera436 <amirah.husbands@gmail.com>
2018-11-10 17:17:30 -0500

    set database helper to format columns in tables
    input validation for database

--- app/src/main/java/edu/spelman/spelfitscmail/spelfit/activities/LoginActivity.java
+++ app/src/main/java/edu/spelman/spelfitscmail/spelfit/activities/LoginActivity.java
@@ -1,15 +1,90 @@
 package edu.spelman.spelfitscmail.spelfit.activities;
 
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
 
 import edu.spelman.spelfitscmail.spelfit.R;
+import edu.spelman.spelfitscmail.spelfit.helper.InputValidation;
+import edu.spelman.spelfitscmail.spelfit.sql.DatabaseHelper;
 
-public class LoginActivity extends AppCompatActivity {
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final AppCompatActivity activity = LoginActivity.this;
+
+    private NestedScrollView nestedScrollView;
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputEditText textInputEditTextPassword;
+
+    private AppCompatButton appCompatButtonLogin;
+    private AppCompatTextView textViewLinkRegister;
+    private InputValidation InputValidation;
+    private DatabaseHelper databaseHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        initViews();
+        initListeners;
+        initObjects();
+    }
+    private void initViews(){
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
+
+        textInputLayoutEmail = (TextInputLayout) findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPassword = (TextInputLayout) findViewById(R.id.textInputLayoutPassword);
+
+        textInputEditTextEmail = (TextInputEditText) findViewById(R.id.textInputEditTextEmail);
+        textInputEditTextPassword = (TextInputEditText) findViewById(R.id.textInputEditTextPassword);
+
+        appCompatButtonLogin = (AppCompatButton) findViewById(R.id.appCompatButtonLogin);
+        textViewLinkRegister = (AppCompatTextView) findViewById(R.id.textViewLinkRegister);
+
+    }
+    private void initListeners(){
+        appCompatButtonLogin.setOnClickListener(this);
+        textViewLinkRegister.setOnClickListener(this);
+    }
+    private void initObjects(){
+        databaseHelper = new DatabaseHelper(activity);
+        inputValidation = new InputValidation(activity);
+
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.appCompatButtonLogin;
+                verifyFromSQLite();
+                break;
+            case R.id.textViewLinkRegister
+                Intent intentRegister = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intentRegister);
+                break;
+        }
+    }
+    private void verifyFromSQLite(){
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextEmail, textInputLayoutEmail, getString(R.string.error_message_email))){
+            return;
+        }
+        if (!inputValidation.isInputEditTextEmail(textInputEditTextEmail, textInputLayoutEmail, getString(R.string.error_message_email))){
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextPassword, textInputLayoutPassword, getString(R.string.error_message_email))){
+            return;
+        }
+        if (databaseHelper.checkUser(textInputEditTextEmail.getText().toString().trim(), textInputEditTextPassword.getText().toString().trim())){
+            Intent accountsIntent
+        }
     }
 }
