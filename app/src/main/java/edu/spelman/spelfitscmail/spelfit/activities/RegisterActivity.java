620b21de6525ce87a261e91ffc685f394eaccf65
meera436 <amirah.husbands@gmail.com>
2018-11-10 18:49:32 -0500

    Register Activity variables

--- app/src/main/java/edu/spelman/spelfitscmail/spelfit/activities/RegisterActivity.java
+++ app/src/main/java/edu/spelman/spelfitscmail/spelfit/activities/RegisterActivity.java
@@ -1,4 +1,38 @@
 package edu.spelman.spelfitscmail.spelfit.activities;
 
-public class RegisterActivity {
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
+
+import edu.spelman.spelfitscmail.spelfit.helper.InputValidation;
+import edu.spelman.spelfitscmail.spelfit.model.User;
+import edu.spelman.spelfitscmail.spelfit.sql.DatabaseHelper;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private final AppCompatActivity activity = RegisterActivity.this;
+
+    private NestedScrollView nestedScrollView;
+
+    private TextInputLayout textInputLayoutName;
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+    private TextInputLayout textInputLayoutConfirmPassword;
+
+    private TextInputEditText textInputEditTextName;
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputEditText textInputEditTextPassword;
+    private TextInputEditText textInputEditTextConfirmPassword;
+
+    private AppCompatButton appCompatButtonRegister;
+    private AppCompatTextView appCompatTextViewLoginLink;
+
+    private InputValidation inputValidation;
+    private DatabaseHelper databaseHelper;
+    private User user;
+
 }
