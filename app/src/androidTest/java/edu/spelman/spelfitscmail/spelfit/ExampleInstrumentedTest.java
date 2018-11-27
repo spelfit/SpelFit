57ab741078dd31e3c0d111901851110b69213109
meera436 <amirah.husbands@gmail.com>
2018-11-27 01:04:56 -0500

    Build successful

--- app/src/androidTest/java/edu/spelman/spelfitscmail/spelfit/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/spelman/spelfitscmail/spelfit/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("edu.spelman.spelfitscmail.spelfit", appContext.getPackageName());
+        assertNotEquals("edu.spelman.spelfitscmail.spelfit", appContext.getPackageName());
     }
 }
